4936c63e7be986593993ea9ed305a056e64bace1
onyekachila <onyekachila@gmail.com>
2018-10-03 14:27:40 +0100

    Added customer model and configuration

---
+++ TelemagicWorkflow/DataLayer/CustomerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using TelemagicWorkflow.Models;
+
+namespace TelemagicWorkflow.DataLayer
+{
+    public class CustomerConfiguration : EntityTypeConfiguration<Customer>
+    {
+        public CustomerConfiguration()
+        {
+            Property(c => c.AccountNumber)
+                .HasMaxLength(8)
+                .IsRequired()
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("AK_Customer_AccountNumber") { IsUnique = true }));
+
+            Property(c => c.Address).HasMaxLength(30).IsRequired();
+            Property(c => c.CompanyName).HasMaxLength(30).IsRequired();
+            Property(C => C.City).HasMaxLength(15).IsRequired();
+            Property(c => c.State).HasMaxLength(2).IsRequired();
+            Property(c => c.ZipCode).HasMaxLength(10).IsRequired();
+            Property(c => c.Phone).HasMaxLength(15).IsOptional(); 
+        }
+    }
+}
