ca8b332f1fb8938e778a09a204e6e1f5b9bb784f
onyekachila <onyekachila@gmail.com>
2018-10-03 14:47:56 +0100

    Added Workder model and workder configurations

---
+++ TelemagicWorkflow/DataLayer/WorkOrderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using TelemagicWorkflow.Models;
+
+namespace TelemagicWorkflow.DataLayer
+{
+    public class WorkOrderConfiguration : EntityTypeConfiguration<WorkOrder>
+    {
+        public WorkOrderConfiguration()
+        {
+            Property(wo => wo.OrderDateTime).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+            Property(wo => wo.Description).HasMaxLength(15).IsOptional();
+            Property(wo => wo.Total).HasPrecision(18, 2).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+            Property(wo => wo.CertificationRequirements).HasMaxLength(120).IsOptional(); 
+        }
+    }
+}
